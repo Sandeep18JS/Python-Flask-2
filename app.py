a955637e8bcccf1c35ae1fd5542f876f539a66a7
Sandeep18JS <125888503+Sandeep18JS@users.noreply.github.com>
2023-06-28 06:02:24 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 from flask import Flask, render_template, jsonify
 
+from database import load_jobs_from_db
+
 app = Flask(__name__)
 
-JOBS = [{
-  'id': 1,
-  'title': 'Data analyst',
-  'location': 'Hyderabad,India',
-  'salary': 'Rs. 10,00,000'
-}, {
-  'id': 2,
-  'title': 'Data Scientist ',
-  'location': 'Delhi,India',
-  'salary': 'Rs. 15,00,000'
-}, {
-  'id': 3,
-  'title': 'Fronend engineer ',
-  'location': 'Bangalore,India',
-  'salary': 'Rs. 12,00,000'
-}, {
-  'id': 4,
-  'title': 'Backend engineer ',
-  'location': 'Chennai,India',
-  'salary': 'Rs. 14,00,000'
-}]
+# JOBS = [{
+#   'id': 1,
+#   'title': 'Data analyst',
+#   'location': 'Hyderabad,India',
+#   'salary': 'Rs. 10,00,000'
+# }, {
+#   'id': 2,
+#   'title': 'Data Scientist ',
+#   'location': 'Delhi,India',
+#   'salary': 'Rs. 15,00,000'
+# }, {
+#   'id': 3,
+#   'title': 'Fronend engineer ',
+#   'location': 'Bangalore,India',
+#   'salary': 'Rs. 12,00,000'
+# }, {
+#   'id': 4,
+#   'title': 'Backend engineer ',
+#   'location': 'Chennai,India',
+#   'salary': 'Rs. 14,00,000'
+# }]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', jobs=JOBS)
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,44 @@
+from sqlalchemy import create_engine, text
+import os
+
+# Create the database connection URL with SSL parameters
+db_url = os.environ['DB_URL']
+
+# Create the SQLAlchemy engine
+engine = create_engine(db_url,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
+
+
+# with engine.connect() as conn:
+#     result = conn.execute(text("select * from jobs"))
+
+#     result_dicts = []
+#     for row in result:
+#         result_dict = dict(row._asdict())
+#         result_dicts.append(result_dict)
+
+#     print(result_dicts)
+
+# print("type(result):", type(result))
+# result_all = result.fetchall()
+
+# print("type(result_all):", type(result_all))
+# first_result = result_all[0]
+
+# print("type(first_result):", type(first_result))
+# first_result_dict = first_result._asdict()
+
+# print("type(first_result_dict):",
+# type(first_result_dict))
+# print(first_result_dict)
